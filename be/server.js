3b94074a6bd67c6c2d0a386cb4d1cab87dc4e163
Keith Charles <keith.k.charles@gmail.com>
2024-05-06 17:34:02 -0600

    add user and events routes

--- be/server.js
+++ be/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { Sequelize } from 'sequelize';
 import { defineModels } from './db/models.js';
 import { seedTables } from './db/seeds.js';
+import { userRoutes } from './routes/users.js';
+import { eventRoutes } from './routes/events.js';
 
 const app = express();
 const port = 3000;
@@ -9,6 +11,9 @@ const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'db/db.sqlite',
 });
+
+app.use('/users', userRoutes);
+app.use('/events', eventRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
