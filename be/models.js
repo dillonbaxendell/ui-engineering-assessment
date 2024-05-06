601201243021b94e64f108d8496e311e60353646
Keith Charles <keith.k.charles@gmail.com>
2024-05-06 15:46:06 -0600

    set up initial models and seeds

---
+++ be/models.js
@@ -0,0 +1,87 @@
+import { DataTypes } from 'sequelize';
+
+export const defineModels = async (sequelize) => {
+  const Users = sequelize.define('users',
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      emailAddress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+    },
+    {
+      indexes: [
+        {
+          fields: ['emailAddress'],
+          unique: true,
+        },
+      ],
+    },
+  );
+
+  const Events = sequelize.define('events', 
+    {
+      name: {
+        type: DataTypes.STRING,
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      startDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+      },
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+    },
+    {
+      indexes: [{ fields: ['userId'] }],
+    }
+  );
+
+  const Attendees = sequelize.define('attendees', {},  {
+    indexes: [
+      {
+        fields: ['userId', 'eventId'],
+      },
+      {
+        fields: ['eventId'],
+      },
+      {
+        fields: ['userId'],
+      },
+    ],
+  });
+  
+  Users.hasMany(Events);
+  Events.belongsTo(Users);
+
+  Users.belongsToMany(Events, { through: 'attendees' });
+  Events.belongsToMany(Users, { through: 'attendees' });
+
+  try {
+    await sequelize.sync({ force: true });
+    console.log('Tables created');
+  } catch (error) {
+    console.error('Unable to create tables:', error);
+  }
+};
--- be/server.js
+++ be/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { Sequelize, DataTypes } from 'sequelize';
-
+import { Sequelize } from 'sequelize';
+import { defineModels } from './models.js';
+import { seedTables } from './seeds.js';
 
 const app = express();
 const port = 3000;
@@ -17,100 +18,13 @@ app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-    defineModels(sequelize);
-  })
-  .catch((error) => {
-    console.error('Unable to connect to the database:', error);
-  });
-
-
-const defineModels = async (s) => {
-  const Users = s.define('users',
-    {
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      emailAddress: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-    },
-    {
-      indexes: [
-        {
-          fields: ['emailAddress'],
-          unique: true,
-        },
-      ],
-    },
-  );
-
-  const Events = s.define('events', 
-    {
-      name: {
-        type: DataTypes.STRING,
-      },
-      location: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      startDate: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      description: {
-        type: DataTypes.TEXT,
-      },
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-    },
-    {
-      indexes: [{ fields: ['userId'] }],
-    }
-  );
-
-  const Attendees = s.define('attendees', {},  {
-    indexes: [
-      {
-        fields: ['userId', 'eventId'],
-      },
-      {
-        fields: ['eventId'],
-      },
-      {
-        fields: ['userId'],
-      },
-    ],
-  });
-  
-  Users.hasMany(Events);
-  Events.belongsTo(Users);
-
-  Users.belongsToMany(Events, { through: 'attendees' });
-  Events.belongsToMany(Users, { through: 'attendees' });
-
-  await s.sync({ force: true })
-  .then(() => {
-    console.log('Tables created');
-  })
-  .catch((error) => {
-    console.error('Unable to create tables:', error);
-  });
-};
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+  await defineModels(sequelize);
+  console.log('Tables created');
+  await seedTables(sequelize);
+  console.log('Tables seeded');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
