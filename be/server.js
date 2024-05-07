0d490f428df425fe2011f616d4e8abae77697ff8
Keith Charles <keith.k.charles@gmail.com>
2024-05-06 20:06:45 -0600

    add login endpoint

--- be/server.js
+++ be/server.js
@@ -8,11 +8,28 @@ import { eventRoutes } from './routes/events.js';
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use('/v1/users', userRoutes);
 app.use('/v1/events', eventRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/v1/login', async (req, res) => {
+  console.log('body', req.body);
+  const user = await sequelize.models.user.findOne({
+    where: {
+      emailAddress: req.body.emailAddress,
+    },
+  });
+
+  if (!user) {
+    res.status(400).send('User not found.');
+  }
+
+  res.json(user);
 });
 
 app.listen(port, () => {
