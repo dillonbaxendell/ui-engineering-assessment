220bad5e0fca66b33f293fbfaafb516f95bf0307
Keith Charles <keith.k.charles@gmail.com>
2024-05-07 22:51:53 -0600

    refactor: use Models.User and status 200

--- be/routes/users.js
+++ be/routes/users.js
@@ -4,7 +4,6 @@ import { Models } from '../db/models.js';
 
 export const userRoutes = express.Router();
 
-const sequelize = new Sequelize('sqlite::memory:');
 const userParams = (body) => ({
   firstName: body.firstName,
   lastName: body.lastName,
@@ -13,36 +12,47 @@ const userParams = (body) => ({
 
 // GET /users
 userRoutes.get('/', async (req, res) => {
-  const users = await sequelize.models.user.findAll();
+  const users = await Models.User.findAll();
 
   if (!users) {
     res.send('No users have been created.');
   }
 
-  res.json(users);
+  res.status(200).json(users);
 });
 
 // GET /users/:id
 userRoutes.get('/:id', async (req, res) => {
-  const user = await Models.User.findByPk(req.params.id);
+  const user = await Models.User.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Models.Event,
+        as: 'attendingEvents',
+      },
+    ],
+  
+  });
 
   if (!user) {
     res.status(404).send('User not found.');
   }
 
-  res.json(user);
+  res.status(200).json(user);
 });
 
 // POST /users
 userRoutes.post('/', async (req, res) => {
-  const user = await sequelize.models.user.create(userParams(req.body));
+  const user = await Models.User.create(userParams(req.body));
 
   res.json(user);
 });
 
 // DELETE /users/:id
 userRoutes.delete('/:id', async (req, res) => {
-  await sequelize.models.user.destroy({
+  await Models.User.destroy({
     where: {
       id: req.params.id,
     },
@@ -53,7 +63,7 @@ userRoutes.delete('/:id', async (req, res) => {
 
 // PATCH /users/:id
 userRoutes.patch('/:id', async (req, res) => {
-  const user = await sequelize.models.user.findByPk(req.params.id);
+  const user = await Models.User.findByPk(req.params.id);
 
   if (!user) {
     res.status(404).send('User not found.');
@@ -66,28 +76,28 @@ userRoutes.patch('/:id', async (req, res) => {
 
 // POST /users/:id/events/:eventId
 userRoutes.post('/:id/events/:eventId', async (req, res) => {
-  const user = await sequelize.models.user.findByPk(req.params.id);
-  const event = await sequelize.models.event.findByPk(req.params.eventId);
+  const user = await Models.User.findByPk(req.params.id);
+  const event = await Models.Event.findByPk(req.params.eventId);
 
   if (!user || !event) {
     res.status(404).send('User or event not found.');
   }
 
-  await user.addEvent(event);
+  await user.addAttendingEvent(event);
 
   res.json(user);
 });
 
 // DELETE /users/:id/events/:eventId
 userRoutes.delete('/:id/events/:eventId', async (req, res) => {
-  const user = await sequelize.models.user.findByPk(req.params.id);
-  const event = await sequelize.models.event.findByPk(req.params.eventId);
+  const user = await Models.User.findByPk(req.params.id);
+  const event = await Models.Event.findByPk(req.params.eventId);
 
   if (!user || !event) {
     res.status(404).send('User or event not found.');
   }
 
-  await user.removeEvent(event);
+  await user.removeAttendingEvent(event);
 
   res.json(user);
 });
