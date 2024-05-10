0dc87d34cc9d24c684f251ab18911fa88b5f7e72
Keith Charles <keith.k.charles@gmail.com>
2024-05-10 12:55:11 -0600

    feat (seeds): add some past dates

--- be/db/seeds.js
+++ be/db/seeds.js
@@ -1,11 +1,13 @@
 import dayjs from 'dayjs';
 
 /**
- * Generates a random day in the future
+ * Generates a random day, in the past or future,
  * within the range of 1 to 30 days.
+ * 
+ * @param {number} direction - 1 for future, -1 for past
  */
-const randomFutureDate = () => {
-  const days = Math.floor(Math.random() * 30) + 1;
+const randomDate = (direction = 1) => {
+  const days = (Math.floor(Math.random() * 30) + 1) * direction;
   return dayjs().add(days, 'day').toDate();
 };
 
@@ -35,25 +37,43 @@ export const seedTables = async (sequelize) => {
       name: 'Meetup at Threeflow',
       location: 'Main Office',
       description: 'Come and meet the team!',
-      startDate: randomFutureDate(),
+      startDate: randomDate(),
     },
     {
       userId: users[0].id,
       name: 'Bug Fixing Party',
       location: 'Fishbowl',
-      startDate: randomFutureDate(),
+      startDate: randomDate(),
     },
     {
-      userId: users[0].id,
+      userId: users[2].id,
       name: 'New Features Project',
       location: 'Greenfield Office',
-      startDate: randomFutureDate(),
+      startDate: randomDate(),
     },
     {
-      userId: users[0].id,
+      userId: users[1].id,
       name: 'Celebration',
       location: 'Main Office',
-      startDate: randomFutureDate(),
+      startDate: randomDate(-1),
+    },
+    {
+      userId: users[2].id,
+      name: 'Team Meeting',
+      location: 'Main Office',
+      startDate: randomDate(-1),
+    },
+    {
+      userId: users[2].id,
+      name: 'Happy Hour',
+      location: 'Office Kitchen',
+      startDate: randomDate(-1),
+    },
+    {
+      userId: users[2].id,
+      name: 'Standup Meeting',
+      location: 'Oval Office',
+      startDate: randomDate(-1),
     },
   ]);
 
